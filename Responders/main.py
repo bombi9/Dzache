4110db469faedda3df3704c5cb037c74d63bbbc9
Zouaghi Mohamed Racem <86516186+bombi9@users.noreply.github.com>
2025-11-26 03:52:30 +0100

    Implement PUT and DELETE request handling
    Added support for PUT and DELETE request types, including response handling.

--- Responders/main.py
+++ Responders/main.py
@@ -1,6 +1,9 @@
 from .GET.Choose import Choose_Response_GET
 # Assuming you will create a HEAD response handler
-from .HEAD.Choose import Choose_Response_HEAD  
+from .HEAD.Choose import Choose_Response_HEAD
+from .HEAD.Choose import Choose_Response_PUT
+from .HEAD.Choose import Choose_Response_DELETE
+
 
 class ChooseAndSend:
     _MaxAge = 90000
@@ -42,11 +45,42 @@ class ChooseAndSend:
             # For HEAD, you might only want headers
             self.serverResponse = response.getHeaders()
         elif self._requestType.upper() == "UPDATE":
-            pass
+            match_put = re.search(r"^PUT\s+(/[\w\.\-]*)", message)
+            if match_put:
+                link = match_put.group(1)
+                file_path = os.path.join(BASE, link[1:])
+
+      
+            data = message.split("\r\n\r\n", 1)
+            body = data[1] if len(data) > 1 else ""
+
+            with open(file_path, "w", encoding="utf-8") as f:
+                f.write(body)
+
+            response = "HTTP/1.1 200 OK\r\n\r\nFile saved."
+            connection.send(response.encode("utf-8"))
+        elif self._requestType.upper() == "PUT":
+            response = Choose_Response_PUT(
+                maxage=ChooseAndSend._MaxAge,
+                requestlnk=self.__requestLink,
+                rl=self._role,
+                uc=self._userCount,
+                message=self._rawMessage
+            )
+            self.serverResponse = response.getResponse()
+
         elif self._requestType.upper() == "DELETE":
-            pass
+            response = Choose_Response_DELETE(
+                maxage=ChooseAndSend._MaxAge,
+                requestlnk=self.__requestLink,
+                rl=self._role,
+                uc=self._userCount,
+                message=self._rawMessage
+            )
+            self.serverResponse = response.getResponse()
+
         else:
-            pass
+            response = "Not, a supported request"
 
     def getResponse(self):
         return self.serverResponse
